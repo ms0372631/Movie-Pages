1f96c3a505d244ab303b216a8cf04c07797ae3b2
Alan Chiu <ms0372631@gmail.com>
2022-06-18 21:16:27 -0700

    navvar hover effect

--- src/index.js
+++ src/index.js
@@ -9,14 +9,22 @@ fetch(popularMovieUrl)
     const movieContent = document.getElementsByClassName("movie-content")[0];
     data.results.forEach(element => {
       console.log(element);
-      const movie = document.createElement("p");
+      const movie = document.createElement("div");
       const image = document.createElement("img")
-      image.src = `https://image.tmdb.org/t/p/original${element.backdrop_path}`
-      const text = document.createTextNode(`${element.original_title}`)
-      movie.appendChild(text);
+      const movieName = document.createElement("p");
+      const movieDate = document.createElement("p");
+      image.src = `https://image.tmdb.org/t/p/original${element.poster_path}`
+      const nametxt = document.createTextNode(`${element.original_title}`)
+      const datetxt = document.createTextNode(`${element.release_date}`)
+      movie.classList.add("movie");
+      movieName.classList.add("movie-name");
+      movieDate.classList.add("movie-date");
       movie.appendChild(image);
+      movie.appendChild(movieName);
+      movie.appendChild(movieDate);
       movieContent.appendChild(movie);
-
+      movieName.appendChild(nametxt);
+      movieDate.appendChild(datetxt);
     });
   })
   .catch(function (err) {
