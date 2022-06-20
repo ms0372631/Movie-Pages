ea5cac73348847c28c19d9178fc5fd942dccbf9d
Alan Chiu <ms0372631@gmail.com>
2022-06-19 17:49:35 -0700

    modal background image

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@
 window.addEventListener("DOMContentLoaded", () => {
   let currentPage = 1;
   let popularMovieUrl = `https://api.themoviedb.org/3/movie/popular?api_key=5777966c56b415716d3ed40933493146&language=en-US&page=${currentPage}`;
+  let likedList = [];
 
   function fetchMovies(url) { 
     fetch(url)
@@ -10,6 +11,7 @@ window.addEventListener("DOMContentLoaded", () => {
       return response;
     })
     .then(function (data) {
+      let map = new Map();
       const movieTable = document.getElementsByClassName("movie-table")[0];
       movieTable.removeChild(movieTable.lastChild);
       const movieContent = document.createElement("div");
@@ -17,6 +19,7 @@ window.addEventListener("DOMContentLoaded", () => {
       movieTable.appendChild(movieContent);
       data.results.forEach(element => {
         // console.log(element);
+        map.set(element.original_title, element);
         const movie = document.createElement("div");
         const image = document.createElement("img")
         const movieName = document.createElement("p");
@@ -33,6 +36,12 @@ window.addEventListener("DOMContentLoaded", () => {
         movieContent.appendChild(movie);
         movieName.appendChild(nametxt);
         movieDate.appendChild(datetxt);
+        movie.addEventListener("click", () => {
+        const modal = document.getElementsByClassName("modal")[0];
+          modal.style.visibility = "visible";
+          document.getElementsByClassName("modal-bg")[0].style.backgroundImage = `url("https://image.tmdb.org/t/p/original${element.backdrop_path}")`;
+          document.getElementsByClassName("")
+        })
       });
     })
     .catch(function (err) {
@@ -47,16 +56,23 @@ window.addEventListener("DOMContentLoaded", () => {
 
   nextBtn.addEventListener("click", () => {
     currentPage += 1;
+    if (currentPage != 1)
+      document.getElementById("prev").style.opacity = "1";
+    document.getElementsByClassName("selector-txt")[0].innerText = `Page ${currentPage} / Total 500 of 100,000 results`;
     popularMovieUrl = `https://api.themoviedb.org/3/movie/popular?api_key=5777966c56b415716d3ed40933493146&language=en-US&page=${currentPage}`;
     fetchMovies(popularMovieUrl);
   })
 
   prevBtn.addEventListener("click", () => {
-    currentPage -= 1;
-    popularMovieUrl = `https://api.themoviedb.org/3/movie/popular?api_key=5777966c56b415716d3ed40933493146&language=en-US&page=${currentPage}`;
-    fetchMovies(popularMovieUrl);
+    if (currentPage > 1) {
+      currentPage -= 1;
+      if (currentPage == 1)
+        document.getElementById("prev").style.opacity = "0.3";
+      document.getElementsByClassName("selector-txt")[0].innerText = `Page ${currentPage} / Total 500 of 100,000 results`;
+      popularMovieUrl = `https://api.themoviedb.org/3/movie/popular?api_key=5777966c56b415716d3ed40933493146&language=en-US&page=${currentPage}`;
+      fetchMovies(popularMovieUrl);
+    }
   })
-
   
 
 });
